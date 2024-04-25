f3265c67afc3887675227de20bc8cedbd5dc6b94
mr-kwansa <a.kwansaamponsah@stu.ucc.edu.gh>
2024-04-25 19:57:51 +0000

    chnaged the smtp file small

--- access_control/info.py
+++ access_control/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS=True,
 EMAIL_HOST='smtp.gmail.com',
 EMAIL_HOST_USER='access_control.demo.django.login@gmail.com',
-EMAIL_HOST_PASSWORD='albertis@school.com',
+EMAIL_HOST_PASSWORD='123456789@!@',
 EMAIL_PORT=587 
