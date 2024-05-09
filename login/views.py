7511167ff31cf647da51263a1b930bf7ef03e6f1
mr-kwansa <a.kwansaamponsah@stu.ucc.edu.gh>
2024-05-09 18:58:54 +0000

    testing smtp

--- login/views.py
+++ login/views.py
@@ -83,24 +83,33 @@ def signup(request):
         send_mail(subject, message,from_email,to_email)
         print (settings.EMAIL_HOST_USER)
         # activation of email for user request)ser
-        current_site = get_current_site(request)
-        email_subject ="Welcome "+myuser.first_name+ ", please click the link below to activate your account"
-        email_message = render_to_string('email_confirmation.html', {
-                'user': myuser.first_name,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
-                'token': generatetoken.make_token(myuser),
-            })
+        # current_site = get_current_site(request)
+        # email_subject ="Welcome "+myuser.first_name+ ", please click the link below to activate your account"
+        # email_message = render_to_string('email_confirmation.html', {
+        #         'user': myuser.first_name,
+        #         'domain': current_site.domain,
+        #         'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+        #         'token': generatetoken.make_token(myuser),
+        #     })
         
-        email = EmailMessage(
-                email_subject,
-                email_message,
-                from_email,
-                to_email,
-        )
-        email.send()
+        # email = EmailMessage(
+        #         email_subject,
+        #         email_message,
+        #         from_email,
+        #         to_email,
+        # )
+        # send_mail(email_subject,email_message,to_email,from_email)
+        # return redirect('signin')
+        
+        subject_activate= "Your account has to  be activated"
+        message_activate= "Hello " + myuser.first_name+ "!"+"\n"+"Please click the link bellow to activate your accoount"
+        + "\n" +render_to_string('email_confirmation.html',{
+            'domain':get_current_site(request),
+            'uid':urlsafe_base64_encode(force_bytes(myuser.uid)),
+            'token':generatetoken.make_token(myuser),
+        })
+        send_mail(subject_activate,message_activate,settings.EMAIL_HOST_USER,to_email,fail_silently=False)
         return redirect('signin')
-    
     return render(request, "login/signup.html")
 
 # signing view
